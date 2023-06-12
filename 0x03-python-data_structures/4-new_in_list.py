d0d87d4ccf40d63a7d2d0b1c384169c9b7852e37
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-06-12 08:27:34 -0700

    Number 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        new = my_list.copy()
+        return new
+    elif idx + 1 > len(my_list):
+        new = my_list.copy()
+        return new
+    else:
+        new = my_list.copy()
+        new[idx] = element
