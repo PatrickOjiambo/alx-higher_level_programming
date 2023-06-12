889f5ef77ec85eae461bd3df8c01c4010e6a9012
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-06-12 11:36:15 -0700

    Advanced

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,16 @@
+#include <Python.h>
+void print_python_list_info(PyObject* p) {
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t i;
+
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject*)p)->allocated);
+
+    for (i = 0; i < size; i++) {
+        PyObject* element = PyList_GetItem(p, i);
+        const char* typeName = Py_TYPE(element)->tp_name;
+
+        printf("Element %ld: %s\n", i, typeName);
+    }
+}
+
