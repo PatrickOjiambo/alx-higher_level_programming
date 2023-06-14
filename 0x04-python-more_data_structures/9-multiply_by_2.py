76df13c32349adbd9796ad5783e20ecb3126523b
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-06-14 11:41:02 -0700

    Number 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dict = {}
-    for key, value in a_dictionary:
+    for key, value in a_dictionaryi.items():
         new_val = value * 2
         new_dict[key] = new_val
     return new_dict
