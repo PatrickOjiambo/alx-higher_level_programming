a072ee7e7045bb0ce14593cbc7fbf46a85c823d2
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-06-14 10:33:25 -0700

    Number 3

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -8,8 +8,9 @@ def reduce(function, iterable, initializer=None):
     for element in it:
         value = function(value, element)
     return value
-def uniq_add(my_list=[]):
 
+
+def uniq_add(my_list=[]):
     new_list = set(my_list)
     final_value = reduce(lambda x, y: x + y, new_list)
     return final_value
