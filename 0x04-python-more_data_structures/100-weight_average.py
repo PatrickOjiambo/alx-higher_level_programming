7e7ab4e492bb2583b5ca9cac3006cbbec4aad9a5
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-06-14 13:10:20 -0700

    number 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    total_weight = 0
+    weighted_sum = 0
+    for score, weight in my_list:
+        total_weight += weight
+        weighted_sum += score * weight
+    return weighted_sum / total_weight
