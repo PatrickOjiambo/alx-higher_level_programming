ed7130e076ccacc9130184e3f01ec0bfb2fe919a
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-06-14 11:21:59 -0700

    Number 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    response = a_dictionary.get(key, 1)
+    if response == 1:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    
+    return a_dictionary
