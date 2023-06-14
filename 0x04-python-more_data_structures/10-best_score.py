53993238467abac25b56d672353780b3d55d0fcc
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-06-14 12:13:10 -0700

    number 11

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     max_value = 0
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     else:
         for key, value in a_dictionary.items():
