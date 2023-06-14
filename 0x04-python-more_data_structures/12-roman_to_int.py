787a2ff94615f66648e203b6c4ab4e8eff55ca3d
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-06-14 12:55:15 -0700

    Number 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    sum = 0
-    for i in roman_string:
-        if i == 'I':
-            i = 1
-        elif i == 'X':
-            i = 10
-        elif i == 'V':
-            i = 5
-        elif i == 'L':
-            i = 50
-        elif i == 'M':
-            i = 1000
-        elif i == 'C':
-            i = 100
-        elif isinstance(roman_string, str) is False:
-            return None
-        else:
-            i = 500
-        sum = sum + i
-    return sum
+    if isinstance(roman_string, str) is False:
+        return 0
+    elif len(roman_string) == 0:
+        return 0
+    else:
+        sum = 0
+        for i in roman_string:
+            if i == 'I':
+                i = 1
+            elif i == 'X':
+                i = 10
+            elif i == 'V':
+                i = 5
+            elif i == 'L':
+                i = 50
+            elif i == 'M':
+                i = 1000
+            elif i == 'C':
+                i = 100
+            else:
+                i = 500
+            sum = sum + i
+        return sum
