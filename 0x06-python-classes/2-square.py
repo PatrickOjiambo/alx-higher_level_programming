759eb7e5946d4955e11d87a2daa4f486e4fd18fb
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-06-27 09:22:30 -0700

    Fixed number 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""More on this squares to come later"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """This is a square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
