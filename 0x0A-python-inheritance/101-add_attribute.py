44ef21040b7e9a0927d8cf2675789c522b2b5d3a
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-07-10 09:35:43 -0700

    Number 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Just a base class for something.
+"""
+
+
+def add_attribute(obj, attr_name, attr_value):
+    """
+    This function sets the attribute name 
+    and all that not so pleasing stuff
+    """
+    if hasattr(obj, attr_name):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, attr_name, attr_value)
