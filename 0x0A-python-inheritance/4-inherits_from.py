f58146dc8e888374f12bf6d969a46807ec32bbcf
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-07-10 07:33:46 -0700

    Number 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,16 +4,16 @@ Module contains an isinstance function
 """
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """
     Write a function that returns True
     if the object is an instance of, or if
     the object is an instance of a class that
-    inherited from,
+    inherited from or not(directly or indirectly),
     Args:
         obj: an object that is inherited from
         a_class: A class that to verify
     Returns:
             True or false
     """
-    return isinstance(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
