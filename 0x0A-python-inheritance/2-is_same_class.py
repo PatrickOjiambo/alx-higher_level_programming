8f5a88c717d70ac44aefc33ce4e049ce5c058e1a
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-07-10 04:45:09 -0700

    Number 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module contains an isinstance function
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function that returns True if the object is exactly
+     an instance of the specified class ; otherwise False.
+     Args:
+        obj: An object of a class
+        a_class: A class to be verified
+     Returns:
+            True or False
+    """
+    return isinstance(obj, a_class)
