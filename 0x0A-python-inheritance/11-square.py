e8ea5faed9da301837eee63efcb601ecab3a3940
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-07-10 12:48:35 -0700

    number 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module containing an empty class BaseGeometry.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Contains the Square class
+    """
+    def __init__(self, size):
+        """
+        Contains the square construcor
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+    def area(self):
+        """
+        Calculates the area of the square
+        """
+        return self.__size ** 2
+    
+    def __str___(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
