ebff6fae07ef9b0b3dcb581720b767477741796d
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-07-10 07:43:01 -0700

    Number 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module containing an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Class contains some geometry class
+    """
+    raise Exception("area() is not implemented")
