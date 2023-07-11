4f091d0d79a1279c0f4362cec062476d9fcd76de
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-07-11 13:19:01 -0700

    Init commits

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,21 +1,8 @@
 #!/usr/bin/python3
-"""
-Module contains a function
-returns the dictionary description with
-simple data structure (list, dictionary,
-string, integer and boolean) for JSON
-serialization of an object:
-"""
-import json
+"""Defines a Python class-to-JSON function."""
 
 
 def class_to_json(obj):
-    """
-    returns the dictionary description with
-    simple data structure (list, dictionary,
-    string, integer and boolean) for JSON
-    serialization of an object:
-    Args:
-        obj(object): object to check
-    """
-    return json.dumps(obj, default=lambda x: x.__dict__)
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
+
