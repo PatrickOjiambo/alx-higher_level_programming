b0ccce03576119d5a3287db1f6e3680f6d79b43f
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-07-11 05:57:59 -0700

    Number 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A module that reads a text file and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    function that reads a text file
+    (UTF8) and prints it to stdout:
+    Args:
+        Filename(str): Contains the file name as string
+    """
+    with open(filename, 'r') as file:
+        content = file.read
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+Files input and output. There is alsosome JSON here
