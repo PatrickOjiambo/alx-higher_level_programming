10ff9434f1865868f92a33f0a806dea3ef49c002
Patrikey OJ <105768730+PatrickOjiambo@users.noreply.github.com>
2023-07-11 23:22:32 +0300

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,17 +4,14 @@
 
 class Student:
     """Creates student instances"""
-
     def __init__(self, first_name, last_name, age):
         """ Initialize student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
     def to_json(self):
         """ Returns description of the directory"""
         return self.__dict__.copy()
-
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student."""
         if (type(attrs) == list and
@@ -22,7 +19,6 @@ class Student:
             ret = {b: getattr(self, b) for b in attrs if hasattr(self, b)}
             return(ret)
         return self.__dict__
-
     def reload_from_json(self, json):
         """Replace all attributes of the Student"""
         for a, b in json.items():
