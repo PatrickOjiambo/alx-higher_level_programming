38632f0849d1b4a47aaa8356f01125f6ef2954ec
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-07-11 07:30:08 -0700

    number 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module contains a function
+creates an Object from a “JSON file”:
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    creates an Object from a “JSON file”
+    Args:
+        filename(str):name of the file to be written
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = json.loads(file.read())
+        print(content, end="")
+
