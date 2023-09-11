928dc2637d90bd474b380b64a30ef4dde3778ef6
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-09-11 17:42:40 +0300

    Number 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argv = process.argv;
+
+const myval = parseInt(argv[2]);
+if (isNaN(argv[2]))
+{
+  console.log('Not a number')
+}
+else{
+  console.log(`My number ${myval}`)
+}
