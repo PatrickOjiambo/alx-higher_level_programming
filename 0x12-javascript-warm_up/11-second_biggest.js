58500bcd67aaf1250de6afc428d78611a7dbc2d9
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-09-11 19:42:36 +0300

    Number 12

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,9 +3,9 @@ const argv = process.argv;
 if (argv.length < 4) {
   console.log("0");
 } else {
-  let myArr = argv.slice(2,);
+  let myArr = argv.slice(2);
   myArr.sort(function (a, b) {
     return b - a;
   });
-  console.log(myArr[1])
+  console.log(myArr[1]);
 }
