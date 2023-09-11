68373d232e2c3a7975271a04c64db07877466ab9
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-09-11 17:22:01 +0300

    Number 4

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const argv = process.argv
-if (argv.length < 3)
-{
-    console.log("No arguments")
-}
-else{
-    console.log(argv[2])
+const argv = process.argv;
+if (argv.length < 3) {
+  console.log('No arguments');
+} else {
+  console.log(argv[2]);
 }
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const argv = process.argv;
+let value1 = argv[2];
+let value2 = argv[3];
+console.log(`${value1} is ${value2}`);
