894ef679d1ae98711c070254ae3017f63b2306a6
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-09-12 18:41:04 +0300

    No. 4

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let output = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        output = output + 'X';
+      }
+      console.log(output);
+      output = '';
+    }
+  }
+};
