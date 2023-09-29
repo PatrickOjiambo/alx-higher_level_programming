4303955e8f69f2ad9aabf5efbe9d4a555097e35a
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-09-29 18:55:45 +0300

    No 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import urllib.request
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet\
+    .hbtn.io/status") as response:
+        body = response.read()
+        print("Body response:")
+        print(f"    - type: {type(body)}")
+        print(f"    - content: {body}")
+        print(f"    - utf8 content: {body.decode('utf-8')}")
